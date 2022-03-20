675100a843dba31fb9f89bf4b9057e3b6a1ae921
Jonathan Ostrander <jonathanost@gmail.com>
2022-03-19 20:18:10 -0400

    feat: make game length configurable

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 const clientId = process.env.DISCORD_CLIENT_ID;
 const token = process.env.DISCORD_TOKEN;
+const gameLength = parseInt(process.env.GAME_LENGTH || '15');
 
-export { clientId, token };
+export { clientId, token, gameLength };
